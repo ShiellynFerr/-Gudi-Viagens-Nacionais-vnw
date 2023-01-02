a984d6b2516dd20886db149ab6ac9c20058f53cb
Shiellyn Bruna Ferreira <brunashiellyn@gmail.com>
2023-01-02 20:58:39 -0300

    adicionando navbar

---
+++ GudiProject/src/styles/components/NavBar/index.tsx
@@ -0,0 +1,24 @@
+import Button from '../Button-home';
+import styles from './index.module.scss';
+
+const NavBar = () => {
+  return (
+    <div className={styles.header}>
+          <div>
+            <img className={styles.logo} src="./public/img/logo-footer.svg" alt="logo" />
+          </div>
+          <div className={styles.box}>
+            <nav className={styles.nav}>
+               <a className={styles.nav_item} href="#">Sobre</a>
+               <a className={styles.nav_item}  href="#">Beneficios</a>
+               <a className={styles.nav_item}  href="#">Contato</a>
+            </nav>
+            <div>
+            <Button textButton="Agendar" />
+          </div>
+          </div>
+    </div>
+  )
+}
+
+export default NavBar
