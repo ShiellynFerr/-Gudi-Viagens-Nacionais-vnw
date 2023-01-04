b4e0e194d50dacd7e2494602a6288e7af07fb7ef
Shiellyn Bruna Ferreira <brunashiellyn@gmail.com>
2023-01-04 16:29:01 -0300

    atualizando a responsividade do navbar

--- GudiProject/src/components/NavBar/index.tsx
+++ GudiProject/src/components/NavBar/index.tsx
@@ -1,22 +1,26 @@
-;import styles from './index.module.scss';
+import styles from './index.module.scss';
 
 const NavBar = () => {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
           <div>
             <img className={styles.logo} src="./public/img/logo-footer.svg" alt="logo" />
           </div>
           <div className={styles.box}>
+
             <nav className={styles.nav}>
-               <a className={styles.nav_item} href="#">Sobre</a>
-               <a className={styles.nav_item}  href="#">Beneficios</a>
-               <a className={styles.nav_item}  href="#">Contato</a>
+            <ul className={styles.ul}>
+                   <li><a className={styles.nav_item} href="#">Sobre</a></li>
+                   <li><a className={styles.nav_item}  href="#">Beneficios</a></li>
+                   <li><a className={styles.nav_item}  href="#">Contato</a></li>
+                   </ul>
             </nav>
             <div>
-           <button>Agendar</button>
+           <button className={styles.button}>Agendar</button>
           </div>
           </div>
-    </div>
+          
+    </header>
   )
 }
 
